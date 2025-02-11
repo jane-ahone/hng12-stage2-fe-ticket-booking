bee3d34a84a8165b2fd0c30fec7c5b2a4f9f30ef
Jane Ahone <jane.eloundou@gmail.com>
2025-02-11 20:44:29 +0100

    remove unnecesary lines causing deployment to fail

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import "./App.css";
-import AttendeeDetails from "./pages/AttendeeDetails";
 import TicketReady from "./pages/TicketReady";
-import TicketSelection from "./pages/TicketSelection";
 
 function App() {
   return (
     <div>
-      {/* <TicketSelection /> */}
-      {/* <AttendeeDetails /> */}
       <TicketReady />
     </div>
   );
--- src/components/Poster.tsx
+++ src/components/Poster.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./Poster.css";
 
 const Poster = () => {
